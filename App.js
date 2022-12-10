4084f8924002e09a8a55ce52e34e903e1c7e0030
codiku-dev <codiku.dev@gmail.com>
2022-12-10 18:44:36 +0100

    wip

---
+++ App.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+import { SafeAreaView } from "react-native-safe-area-context";
+import { s } from "./App.style";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+}
