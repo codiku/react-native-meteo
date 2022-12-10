ac6eb6258be4cd4c8f60f8b6123a7c44bc856bdf
codiku-dev <codiku.dev@gmail.com>
2022-12-10 22:11:43 +0100

    wip

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import * as Location from "expo-location";
+
 import { Text, View } from "react-native";
 import { useEffect, useState } from "react";
 
@@ -12,20 +14,40 @@ const API_URL =
   "https://api.open-meteo.com/v1/forecast?latitude=48.85&longitude=2.35&daily=weathercode,temperature_2m_max,sunrise,sunset,windspeed_10m_max&timezone=Europe%2FBerlin&current_weather=true";
 
 export default function App() {
+  const [coordinates, setCoordinates] = useState();
   const [weatherData, setWeatherData] = useState();
   const [isFontLoaded] = useFonts({
     "Alata-Regular": require("./assets/fonts/Alata-Regular.ttf"),
   });
 
+  useEffect(() => {
+    getUserCoordinates();
+  }, []);
+
+  useEffect(() => {
+    if (coordinates) {
+      fetchWeather();
+    }
+  }, [coordinates]);
+
   async function fetchWeather() {
     const weatherResponse = await axios.get(API_URL);
     setWeatherData(weatherResponse.data);
   }
-  useEffect(() => {
-    //  fetchWeather();
-  }, []);
+  async function getUserCoordinates() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      // Default to Paris
+      setCoordinates({ lat: "48.85", lng: "2.35" });
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    setCoordinates({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
+  }
 
-  console.log(weatherData);
   return isFontLoaded && weatherData ? (
     <SafeAreaView style={s.container}>
       <View style={s.meteo_container}>
