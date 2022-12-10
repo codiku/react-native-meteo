30ca4cbcd372e06657df9cedfa64814e5901cb4a
codiku-dev <codiku.dev@gmail.com>
2022-12-10 20:43:29 +0100

    wip

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
 
 import { MeteoBasic } from "./components/MeteoBasic/MeteoBasic";
 import { SafeAreaView } from "react-native-safe-area-context";
+import axios from "axios";
 import { s } from "./App.style";
-import { useEffect } from "react";
 import { useFonts } from "expo-font";
 
+const API_URL =
+  "https://api.open-meteo.com/v1/forecast?latitude=48.85&longitude=2.35&daily=weathercode,temperature_2m_max,sunrise,sunset,windspeed_10m_max&timezone=Europe%2FBerlin&current_weather=true";
+
 export default function App() {
+  const [weatherData, setWeatherData] = useState();
   const [isFontLoaded] = useFonts({
     "Alata-Regular": require("./assets/fonts/Alata-Regular.ttf"),
   });
 
+  async function fetchWeather() {
+    const weatherResponse = await axios.get(API_URL);
+    setWeatherData(weatherResponse.data);
+  }
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
+  console.log(weatherData);
   return isFontLoaded ? (
     <SafeAreaView style={s.container}>
       <View style={s.meteo_container}>
