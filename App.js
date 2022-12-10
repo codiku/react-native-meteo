77274cbf0b9626f8ec17602c1c8f3e55b61ade17
codiku-dev <codiku.dev@gmail.com>
2022-12-10 18:55:33 +0100

    wip

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import { Text, View } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [isFontLoaded] = useFonts({
+    "Alata-Regular": require("./assets/fonts/Alata-Regular.ttf"),
+  });
   return (
     <SafeAreaView>
-      <Text>Hello</Text>
+      {isFontLoaded && (
+        <Text style={{ fontFamily: "Alata-Regular", fontSize: 50 }}>Hello</Text>
+      )}
     </SafeAreaView>
   );
 }
