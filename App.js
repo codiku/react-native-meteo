2152f57d014b63763c4d685bd8616e285ce3ec79
codiku-dev <codiku.dev@gmail.com>
2022-12-10 20:24:42 +0100

    wip

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { Text, View } from "react-native";
 import { MeteoBasic } from "./components/MeteoBasic/MeteoBasic";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
+import { useEffect } from "react";
 import { useFonts } from "expo-font";
 
 export default function App() {
   const [isFontLoaded] = useFonts({
     "Alata-Regular": require("./assets/fonts/Alata-Regular.ttf"),
   });
+
   return isFontLoaded ? (
     <SafeAreaView style={s.container}>
       <View style={s.meteo_container}>
