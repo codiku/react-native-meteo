5027a18991bee6bd64a45d1d74c917a25290698e
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-12-27 18:02:25 +0100

    wip

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,12 +1,50 @@
 import { Text, View } from "react-native";
 import { s } from "./Home.style";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
+import { useEffect, useState } from "react";
+import { MeteoAPI } from "../../api/meteo";
 
-export function Home({}) {
+export function Home() {
+  const [coords, setCoords] = useState();
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+    getUserCoords();
+  }, []);
+
+  useEffect(() => {
+    if (coords) {
+      fetchWeather(coords);
+    }
+  }, [coords]);
+
+  console.log(weather);
+  async function getUserCoords() {
+    let { status } = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoords({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoords({ lat: "48.85", lng: "2.35" });
+    }
+  }
+
+  async function fetchWeather(coords) {
+    const weatherResponse = await MeteoAPI.fetchWeatherFromCoords(
+      coords
+    );
+    setWeather(weatherResponse);
+  }
+
   return (
     <>
-      <View style={s.meteo_basic}>
-        <Text style={{ fontSize: 60, color: "white" }}>Hello</Text>
-      </View>
+      <View style={s.meteo_basic}></View>
       <View style={s.searchbar_container}></View>
       <View style={s.meteo_advanced}></View>
     </>
