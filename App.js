e7492afb8f69d6f28429c7766b735993d2cff674
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-01-04 18:54:55 +0100

    notification

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Forecast } from "./pages/Forecast/Forecast";
+import { useEffect } from "react";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+import { Platform } from "react-native";
 const Stack = createNativeStackNavigator();
 
 const navTheme = {
@@ -16,6 +20,50 @@ export default function App() {
   const [isFontLoaded] = useFonts({
     "Alata-Regular": AlataRegular,
   });
+
+  async function subscribeToNotifications() {
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } =
+          await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      const token = (await Notifications.getExpoPushTokenAsync())
+        .data;
+      // Envoi du token a notre serveur
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+  }
+
+  useEffect(() => {
+    Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log(
+          "data",
+          response.notification.request.content.data
+        );
+      }
+    );
+    subscribeToNotifications();
+  }, []);
+
   return isFontLoaded ? (
     <NavigationContainer theme={navTheme}>
       <Stack.Navigator
