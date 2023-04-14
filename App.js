4c329a8672376530bffbeb7c0bf4a6f874238e89
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-04-14 18:57:44 +0200

    wip

--- App.js
+++ App.js
@@ -3,7 +3,33 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Home } from "./pages/Home/Home";
 import { ImageBackground } from "react-native";
 import backgroundImg from "./assets/background.png";
+import { useEffect, useState } from "react";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
 export default function App() {
+  const [coordinates, setCoordinates] = useState();
+
+  useEffect(() => {
+    getUserCoordinates();
+  }, []);
+
+  async function getUserCoordinates() {
+    const { status } = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoordinates({ lat: "48.85", lng: "2.35" });
+    }
+  }
+
+  console.log(coordinates);
+
   return (
     <ImageBackground
       imageStyle={s.img}
