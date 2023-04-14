5c903091e0a0e403f10da25bf019437d2171a473
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-04-14 17:15:48 +0200

    wip

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { s } from "./App.style";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Home } from "./pages/Home/Home";
+import { ImageBackground } from "react-native";
+import backgroundImg from "./assets/background.png";
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Home />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ImageBackground
+      imageStyle={s.img}
+      style={s.img_background}
+      source={backgroundImg}
+    >
+      <SafeAreaProvider>
+        <SafeAreaView style={s.container}>
+          <Home />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ImageBackground>
   );
 }
--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,5 +1,17 @@
-import { Text } from "react-native";
-
+import { Text, View } from "react-native";
+import { s } from "./Home.style";
 export function Home() {
-  return <Text>Hello from home</Text>;
+  return (
+    <>
+      <View style={s.meteo_basic}>
+        <Text style={s.txt}>Basic Weather info</Text>
+      </View>
+      <View style={s.searchbar_container}>
+        <Text style={s.txt}>SearchBar</Text>
+      </View>
+      <View style={s.meteo_advanced}>
+        <Text style={s.txt}>Advanded Weather info</Text>
+      </View>
+    </>
+  );
 }
