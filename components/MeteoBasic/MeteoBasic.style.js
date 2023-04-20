1fb3487298ba4bd86c928443355d5c6384fd740e
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-04-20 18:15:58 +0200

    Update MeteoBasic.style.js

--- components/MeteoBasic/MeteoBasic.style.js
+++ components/MeteoBasic/MeteoBasic.style.js
@@ -1,11 +1,29 @@
 import { StyleSheet } from "react-native";
 
-export const s = StyleSheet.create({
+const s = StyleSheet.create({
   clock: {
     alignItems: "flex-end",
   },
-  interpretation: {},
-  temperature_box: {},
-  temperature: {},
-  image: {},
+  interpretation: {
+    alignSelf: "flex-end",
+    transform: [{ rotate: "-90deg" }],
+  },
+  interpretation_txt: {
+    fontSize: 20,
+  },
+  image: {
+    height: 50,
+    width: 50,
+    backgroundColor: "white",
+  },
+  temperature_box: {
+    alignItems: "baseline",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  temperature: {
+    fontSize: 150,
+  },
 });
+
+export { s };
