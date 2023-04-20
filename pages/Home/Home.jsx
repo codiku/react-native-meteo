29c448d351832aa69c8979659e2675de8fdd9daf
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-04-20 18:17:16 +0200

    wip

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,12 +1,11 @@
 import { View } from "react-native";
 import { s } from "./Home.style";
 import { Txt } from "../../components/Txt/Txt";
-import { MeteoBasic } from "../../components/MeteoBasic/MeteoBasic";
 export function Home() {
   return (
     <>
       <View style={s.meteo_basic}>
-        <MeteoBasic />
+        <Txt style={{ fontSize: 70 }}>Hello</Txt>
       </View>
       <View style={s.searchbar_container}>
         <Txt>SearchBar</Txt>
