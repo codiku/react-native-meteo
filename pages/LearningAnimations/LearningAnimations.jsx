195b69aec6f6b8d0069300ee29bdea7ecfc8341e
Codiku <66690474+Chill-Studio@users.noreply.github.com>
2023-04-27 20:30:43 +0200

    wip

--- pages/LearningAnimations/LearningAnimations.jsx
+++ pages/LearningAnimations/LearningAnimations.jsx
@@ -1,16 +1,22 @@
-import { useEffect, useState } from "react"
-import { View } from "react-native"
+import { useEffect, useRef, useState } from "react"
+import { Animated, Dimensions, View } from "react-native"
 
 export function LearningAnimations() {
-    const [x, setX] = useState(0)
-
+    const opacity = useRef(new Animated.Value(0)).current;
+    const x = useRef(new Animated.Value(0)).current;
     useEffect(() => {
-        /* setInterval(() => {
-             setX(x => x + 1)
-         }, 25)
-         */
+        Animated.timing(opacity, {
+            toValue: 1,
+            duration: 3000,
+            useNativeDriver: true,
+        }).start();
+        Animated.timing(x, {
+            toValue: 200,
+            duration: 3000,
+            useNativeDriver: true,
+        }).start();
     }, [])
     return <View>
-        <View style={{ backgroundColor: "orange", height: 100, width: 100, transform: [{ translateX: x }] }} />
+        <Animated.View style={{ backgroundColor: "orange", height: 100, width: 100, opacity: opacity, transform: [{ translateX: x }] }} />
     </View>
 }
