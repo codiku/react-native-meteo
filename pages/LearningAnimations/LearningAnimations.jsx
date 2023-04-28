54d3186511d0f4e07351794c6e8ad94c92c836a8
Codiku <66690474+Chill-Studio@users.noreply.github.com>
2023-04-28 12:48:41 +0200

    wip

--- pages/LearningAnimations/LearningAnimations.jsx
+++ pages/LearningAnimations/LearningAnimations.jsx
@@ -1,4 +1,4 @@
-import { Dimensions, View } from "react-native"
+import { Dimensions, SafeAreaView, View } from "react-native"
 import { WaterDrop } from "../../components/WaterDrop/WaterDrop";
 import Animated, {
     useSharedValue,
@@ -10,6 +10,7 @@ import Animated, {
 import { PanGestureHandler } from "react-native-gesture-handler";
 import { useEffect } from "react";
 import { StyleSheet } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 const LEFT_SCREEN = 0
 const RIGHT_SCREEN = Dimensions.get("window").width
 const TOP_SCREEN = 0
@@ -17,35 +18,30 @@ const BOT_SCREEN = Dimensions.get("window").height
 const SQUARE_SIZE = 100
 export function LearningAnimations() {
 
-    const x = useSharedValue(0);
-    /*
-        const gestureHandler = useAnimatedGestureHandler({
-            onStart: (_, ctx) => {
-                ctx.startX = 0;
-            },
-            onActive: (event, ctx) => {
-                x.value = ctx.startX + event.translationX;
-            },
-    
-        });
-         */
+    /* Animations values */
+    const animSquareX = useSharedValue(0);
+    const animSquareOpacity = useSharedValue(1);
+
+    /* Running animations */
     useEffect(() => {
-        x.value = withTiming(RIGHT_SCREEN - SQUARE_SIZE, { duration: 2000 })
+        animSquareX.value = withTiming(RIGHT_SCREEN - SQUARE_SIZE, { duration: 2000 })
+        animSquareOpacity.value = withTiming(0, { duration: 2000 })
     }, [])
 
+    /* Associating animations to style properties */
     const animatedStyle = useAnimatedStyle(() => {
         return {
             transform: [
                 {
-                    translateX: x.value,
+                    translateX: animSquareX.value,
                 },
             ],
+            opacity: animSquareOpacity.value
         };
     });
+    /* Sending the animation style to Animated.View */
+    return <Animated.View style={[s.square, animatedStyle]} />
 
-    return <>
-        <Animated.View style={[s.square, animatedStyle]} />
-    </>
 
 }
 /*
