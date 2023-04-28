c867c47c25a935f6730ba8eaab91e6e02967da89
Codiku <66690474+Chill-Studio@users.noreply.github.com>
2023-04-28 13:07:23 +0200

    Update LearningAnimations.jsx

--- pages/LearningAnimations/LearningAnimations.jsx
+++ pages/LearningAnimations/LearningAnimations.jsx
@@ -3,8 +3,8 @@ import { WaterDrop } from "../../components/WaterDrop/WaterDrop";
 import Animated, {
     useSharedValue,
     withSpring,
+    withRepeat,
     useAnimatedStyle,
-    useAnimatedGestureHandler,
     withTiming,
 } from 'react-native-reanimated';
 import { PanGestureHandler } from "react-native-gesture-handler";
@@ -12,20 +12,24 @@ import { useEffect } from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 const LEFT_SCREEN = 0
-const RIGHT_SCREEN = Dimensions.get("window").width
+const RIGHT_SCREEN = Dimensions.get("screen").width
 const TOP_SCREEN = 0
-const BOT_SCREEN = Dimensions.get("window").height
+const BOT_SCREEN = Dimensions.get("screen").height
 const SQUARE_SIZE = 100
+const HALF_SCREEN_HOR = Dimensions.get("screen").width / 2
+const HALF_SCREEN_VERT = Dimensions.get("screen").height / 2
 export function LearningAnimations() {
 
     /* Animations values */
-    const animSquareX = useSharedValue(0);
+    const animSquareX = useSharedValue(HALF_SCREEN_HOR - SQUARE_SIZE / 2);
     const animSquareOpacity = useSharedValue(1);
+    const animSquareScale = useSharedValue(1);
 
     /* Running animations */
     useEffect(() => {
-        animSquareX.value = withTiming(RIGHT_SCREEN - SQUARE_SIZE, { duration: 2000 })
-        animSquareOpacity.value = withTiming(0, { duration: 2000 })
+        // animSquareX.value = withTiming(RIGHT_SCREEN - SQUARE_SIZE, { duration: 2000 })
+        // animSquareOpacity.value = withSpring(1, { duration: 2000 })
+        animSquareScale.value = withRepeat(withSpring(2, { duration: 5000 }), -1, true)
     }, [])
 
     /* Associating animations to style properties */
@@ -35,7 +39,11 @@ export function LearningAnimations() {
                 {
                     translateX: animSquareX.value,
                 },
+                {
+                    scale: animSquareScale.value
+                }
             ],
+            borderRadius: animSquareScale.value * 30,
             opacity: animSquareOpacity.value
         };
     });
@@ -49,5 +57,5 @@ export function LearningAnimations() {
 */
 
 const s = StyleSheet.create({
-    square: { backgroundColor: 'blue', height: SQUARE_SIZE, width: SQUARE_SIZE }
+    square: { backgroundColor: 'blue', position: "absolute", top: HALF_SCREEN_VERT - (SQUARE_SIZE / 2), height: SQUARE_SIZE, width: SQUARE_SIZE }
 })
