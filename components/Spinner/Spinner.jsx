9f2df6254e560135edf9a19ff056899525a0ecc5
Codiku <66690474+Chill-Studio@users.noreply.github.com>
2023-04-28 22:38:41 +0200

    wip

--- components/Spinner/Spinner.jsx
+++ components/Spinner/Spinner.jsx
@@ -1,4 +1,4 @@
-import { Dimensions, SafeAreaView, View } from "react-native"
+import { Dimensions, SafeAreaView, Text, View } from "react-native"
 import { WaterDrop } from "../../components/WaterDrop/WaterDrop";
 import Animated, {
     useSharedValue,
@@ -11,6 +11,7 @@ import { PanGestureHandler } from "react-native-gesture-handler";
 import { useEffect } from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Txt } from "../../components/Txt/Txt"
 const LEFT_SCREEN = 0
 const RIGHT_SCREEN = Dimensions.get("screen").width
 const TOP_SCREEN = 0
@@ -21,15 +22,15 @@ const HALF_SCREEN_VERT = Dimensions.get("screen").height / 2
 export function LearningAnimations() {
 
     /* Animations values */
-    const animSquareX = useSharedValue(HALF_SCREEN_HOR - SQUARE_SIZE / 2);
     const animSquareOpacity = useSharedValue(1);
-    const animSquareScale = useSharedValue(1);
+    const animSquareRotateX = useSharedValue("0deg");
+    const animSquarebackgroundColor = useSharedValue("#3878F4");
+
 
     /* Running animations */
     useEffect(() => {
-        // animSquareX.value = withTiming(RIGHT_SCREEN - SQUARE_SIZE, { duration: 2000 })
-        // animSquareOpacity.value = withSpring(1, { duration: 2000 })
-        animSquareScale.value = withRepeat(withSpring(2, { duration: 5000 }), -1, true)
+        animSquareRotateX.value = withRepeat(withTiming("360deg", { duration: 3000 }), -1)
+        animSquarebackgroundColor.value = withRepeat(withTiming("red", { duration: 3000 }), -1, true)
     }, [])
 
     /* Associating animations to style properties */
@@ -37,19 +38,18 @@ export function LearningAnimations() {
         return {
             transform: [
                 {
-                    translateX: animSquareX.value,
-                },
-                {
-                    scale: animSquareScale.value
+                    rotate: animSquareRotateX.value
                 }
             ],
-            borderRadius: animSquareScale.value * 30,
-            opacity: animSquareOpacity.value
+            opacity: animSquareOpacity.value,
+            backgroundColor: animSquarebackgroundColor.value
         };
     });
 
     /* Sending the animation style to Animated.View */
-    return <Animated.View style={[s.square, animatedStyle]} />
+    return <Animated.View style={[s.square, animatedStyle]} >
+        <Text style={{ color: "white", fontSize: 10 }}>Loading...</Text>
+    </Animated.View>
 
 
 }
@@ -57,5 +57,5 @@ export function LearningAnimations() {
 */
 
 const s = StyleSheet.create({
-    square: { backgroundColor: 'blue', position: "absolute", top: HALF_SCREEN_VERT - (SQUARE_SIZE / 2), height: SQUARE_SIZE, width: SQUARE_SIZE }
+    square: { borderRadius: 30, justifyContent: 'center', alignItems: 'center', height: SQUARE_SIZE, width: SQUARE_SIZE }
 })
