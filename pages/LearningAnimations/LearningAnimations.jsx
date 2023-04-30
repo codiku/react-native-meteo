4490a871f9d655829b7ba6d8ca080a670873d5e2
Codiku <66690474+Chill-Studio@users.noreply.github.com>
2023-04-30 13:47:20 +0200

    wip

--- pages/LearningAnimations/LearningAnimations.jsx
+++ pages/LearningAnimations/LearningAnimations.jsx
@@ -10,19 +10,19 @@ const H = Dimensions.get("screen").height
 const W = Dimensions.get("screen").width
 //https://rgbacolorpicker.com/
 const colors = ['rgba(245, 39, 39, 0.8)', 'rgba(245, 140, 39, 0.8)', 'rgba(245, 226, 39, 0.8)', 'rgba(39, 245, 97, 0.8)', 'rgba(39, 153, 245, 0.8)', 'rgba(71, 39, 245, 0.8)', 'rgba(245, 39, 238, 0.8)']
-
+const pics = ["https://fastly.picsum.photos/id/1045/200/200.jpg?hmac=NOMPYGOtm89-zlf7NNDG7qSjCOy3XpvrdQRBF4aUZgE", "https://fastly.picsum.photos/id/89/200/200.jpg?hmac=urh6JbqBxFLcQQEPfUQ23bUlEH7vg3qqqUOts86_LLI", "https://fastly.picsum.photos/id/23/200/200.jpg?hmac=IMR2f77CBqpauCb5W6kGzhwbKatX_r9IvgWj6n7FQ7c", "https://fastly.picsum.photos/id/543/200/200.jpg?hmac=YuoZMit51ELMe2bbgwymVwwXFjmpaZ7hS_J1MfjC1IQ", "https://fastly.picsum.photos/id/260/200/200.jpg?hmac=Nu9V4Ixqq3HiFhfkcsL5mNRZAZyEHG2jotmiiMRdxGA", "https://fastly.picsum.photos/id/511/200/200.jpg?hmac=QTzrMGu9nrJDRE4TMoboI_EAM5ZdwXF09ylHr7LFZCg", "https://fastly.picsum.photos/id/596/200/200.jpg?hmac=TiMsstBNF6YKq9Gn7QGsihITEUcv_O8QuTXEVR3T6GA"]
 export function LearningAnimations() {
     const translateX = useSharedValue(0)
     const scrollHandler = useAnimatedScrollHandler((e) => {
         translateX.value = e.contentOffset.x
     })
     return <Animated.ScrollView onScroll={scrollHandler} horizontal style={[s.container]}>
-        {colors.map((c, i) => <ListItem key={c} index={i} color={c} opacity={(i + 1) / 10} translateX={translateX} />)}
+        {colors.map((c, i) => <ListItem pic={pics[i]} key={c} index={i} color={c} opacity={(i + 1) / 10} translateX={translateX} />)}
     </Animated.ScrollView>
 
 }
 
-const ListItem = ({ color, translateX, index }) => {
+const ListItem = ({ color, translateX, index, pic }) => {
     const screenTranslateXInterpolationValues = [(index - 1) * W, index * W, (index + 1) * W]
 
     const animStyle = useAnimatedStyle(() => {
@@ -42,7 +42,7 @@ const ListItem = ({ color, translateX, index }) => {
         }
     })
     return <View style={[s.listItem, { backgroundColor: color }]} >
-        <Animated.View style={[s.square, animStyle]} />
+        <Animated.Image style={[s.square, animStyle]} source={{ uri: pic }} />
     </View>
 }
 
@@ -60,7 +60,9 @@ const s = StyleSheet.create({
     square: {
         height: 200,
         width: 200,
-        backgroundColor: "white"
+        backgroundColor: "white",
+        borderWidth: 2,
+        borderColor: "white"
     }
 
 })
